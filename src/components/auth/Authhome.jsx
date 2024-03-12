68a2ee00aad8fd32e4c79acf85a2e5110d597530
Akash Kumar <akash.kumar@arkasoftwares.com>
2024-03-12 11:40:26 +0530

    install package

---
+++ src/components/auth/Authhome.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Authhome() {
+    return (
+        <>
+            <div className='flex flex-row '>
+                <div className="basis-1/2 p-2">
+                    <img src='/logo.svg' alt='my-logo' className='object-fill h-96 w-96 mt-30 ml-30' />
+                </div>
+                <div className="basis-1/2">02</div>
+            </div >
+        </>
+    )
+}
+
+export default Authhome
